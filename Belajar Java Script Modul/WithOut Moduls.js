91a8a601a90805b6ac7f26f0ac997a2b0558cd13
Mochamad Ridwan <ridwansyah1933@gmail.com>
2022-11-21 13:44:43 +0700

    Project Awal

---
+++ Belajar Java Script Modul/WithOut Moduls.js
@@ -0,0 +1,11 @@
+export function sayHallo(name) {
+    console.info('Halo ${name}')
+}
+
+
+export function sayGoodBye(name) {
+    console.info(`<p>Good Bye ${name}</p>`);
+}
+
+
+const name = "eko";
